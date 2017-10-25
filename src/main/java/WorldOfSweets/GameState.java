21d77ea0008e288f0a0a1c8c79ad9ec564bdcb65
jog93 <joshgel@comcast.net>
2017-10-25 15:03:42 -0400

    added button for determining players

---
+++ src/main/java/WorldOfSweets/GameState.java
@@ -0,0 +1,38 @@
+package WorldOfSweets;
+import javax.swing.*;
+
+
+
+public class GameState{
+	
+	int response = 0;		//Number of total players
+	int totalPlayers = 0;
+	
+	public GameState(){
+		//initialize panel and combo box
+		JPanel panel = new JPanel();
+		panel.add(new JLabel("Welcome to the World-o-Sweets, the GAME of games.  How many people are playing? :"));
+		DefaultComboBoxModel model = new DefaultComboBoxModel();
+		
+		//Add my elements to the dropdown
+		model.addElement("2");
+		model.addElement("3");
+		model.addElement("4");
+		
+		JComboBox selection = new JComboBox(model);
+		panel.add(selection);
+		response = JOptionPane.showConfirmDialog(null, selection, "Number of Players", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE);
+		if (response != JOptionPane.OK_OPTION){
+			System.exit(0);
+		} else {
+			totalPlayers = selection.getSelectedItem();
+		}
+	}
+	
+	
+	public static void main(String args[]) {
+		//ASK HOW MANY PLAYERS
+		GameState gameState = new GameState();
+	}
+}
+
